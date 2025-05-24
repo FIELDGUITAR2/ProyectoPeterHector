f82b06917c69fab4e1a3e39d68207f7da67d6373
root <josagoor@gmail.com>
2025-05-24 18:32:38 -0500

    otro

--- logica/Cuenta.php
+++ logica/Cuenta.php
@@ -83,13 +83,13 @@ class Cuenta
 
         if ($datos) {
             $cuenta = new Cuenta(
-                $datos[0],  // idCuenta
-                $datos[1],  // fechaLimite
-                $datos[2],  // cantidad
-                $datos[3],  // saldoAnterior
-                $datos[4],  // idAdmin
-                $datos[5],  // idApartamento
-                $datos[6]   // idEstadoPago
+                $datos[0], 
+                $datos[1],
+                $datos[2],
+                $datos[3],
+                $datos[4],
+                $datos[5],
+                $datos[6]
             );
             $conexion->cerrar();
             return $cuenta;
